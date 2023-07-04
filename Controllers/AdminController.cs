be3c3ebfdd0d4d72c79e08fe8cc441b4d944a38e
44private44 <goodjob3092001@gmail.com>
2023-07-04 17:56:46 +0530

    Partial View

---
+++ Controllers/AdminController.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Admin_CRUD.Controllers
+{
+    public class AdminController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        //Admin Page
+
+        public IActionResult Admin_Page()
+        {
+            return View();
+        }
+
+        // User Admin Page
+        public IActionResult UserAdmin()
+        {
+            return PartialView("_UserAdmin");
+        }
+
+        // Cms Admin Page
+
+        public IActionResult CMSAdmin()
+        {
+            return PartialView("_CMSAdmin");
+        }
+
+        // Mission Admin Page
+
+        public IActionResult MissionAdmin()
+        {
+            return PartialView("_MissionAdmin");
+        }
+
+        // MissionTheme Admin Page
+
+        public IActionResult MissionThemeAdmin()
+        {
+            return PartialView("_MissionThemeAdmin");
+        }
+
+        // Mission Skill Admin Page
+        public IActionResult MissionSkillAdmin()
+        {
+            return PartialView("_MissionSkillAdmin");
+        }
+
+        // Mission Application Admin Page
+
+        public IActionResult MissionAppAdmin()
+        {
+            return PartialView("_MissionAppAdmin");
+        }
+
+        // Story Admin Page
+
+        public IActionResult StoryAdmin()
+        {
+            return PartialView("_StoryAdmin");
+        }
+
+        // Banner Admin Page
+
+        public IActionResult BannerAdmin()
+        {
+            return PartialView("_BannerAdmin");
+        }
+    }
+}
+
+/////////////////////////////////////////////
---
+++ wwwroot/js/AdminJS.js
@@ -0,0 +1,53 @@
+﻿// update the time in every second
+
+function updateTime() {
+    const now = new Date();
+    const formattedTime = now.toLocaleString("en-US", { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric', hour: 'numeric', minute: 'numeric', hour12: false });
+    $("#current-time").text(formattedTime);
+}
+
+updateTime(); // Call the function once to display the current time
+setInterval(updateTime, 1000); // Call the function every second using setInterval
+
+//--------------------------------------------------------------------------------------------------------------
+// Partial view data for in Partial view
+$(document).ready(function () {
+
+    // default active menu at first time
+
+        var UserActiveLink = $('.default-link');
+        var url = UserActiveLink.attr('href');
+
+        // Load the data for the default active link
+        $.ajax({
+            url: url,
+            type: 'GET',
+            success: function (data) {
+                $('#partialViewContainer').html(data);
+            },
+            error: function () {
+                alert("Something going wrong !");
+            }
+        });
+
+    // when click on the tag or menu
+    $('.admin-menu-link').click(function (e) {
+        e.preventDefault();
+        var url = $(this).attr('href');
+        $('.main_admin_menus_divs a button').removeClass('active');
+
+        $(this).find('button').addClass('active');
+
+        $.ajax({
+            url: url,
+            type: 'GET',
+            success: function (data) {
+                $('#partialViewContainer').html(data);
+            },
+            error: function () {
+                alert("Something going wrong !");
+            }
+        });
+    });
+});
+
